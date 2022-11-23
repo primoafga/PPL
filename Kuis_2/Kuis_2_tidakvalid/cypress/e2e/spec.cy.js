d493a5903797cd51f762149b55913285a7a133f6
primoafga <112939895+primoafga@users.noreply.github.com>
2022-11-23 09:35:44 +0700

    Add files via upload

---
+++ Kuis_2/Kuis_2_tidakvalid/cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    //halaman website
+    cy.visit('https://obefib.arriansoft.com')
+
+    //proses memasukkan data tidak valid
+    cy.get(':nth-child(5) > a > .info-box > .info-box-content > .info-box-number').click()
+    cy.get(':nth-child(2) > .form-control').get("select").select("SASCIN").invoke("val").should("eq", "SASCIN")
+    cy.get(':nth-child(3) > .form-control').type("2022019310092001")
+    cy.get(':nth-child(4) > .form-control').type("sascinjiayou9")
+    cy.get('.btn').click()
+    cy.get(':nth-child(5) > [href="# "]').click()
+    cy.get('.menu-is-opening > .nav > :nth-child(1) > .nav-link').click()
+    cy.get(':nth-child(1) > [width="8%"] > .AspekBtn').click()
+    cy.get('div.col-sm-3 > .input-group > .form-control').type("1000")
+    cy.get('#AspekForm > .modal-footer > [type="submit"]').click()
+  })
+})
